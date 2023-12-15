8ced37c9520a45f12e25b4c8845846d833f34c8d
autroshot <autroshot@gmail.com>
2023-12-15 20:06:50 +0900

    DFS 알고리즘 수정

--- 알고리즘/dfs.js
+++ 알고리즘/dfs.js
@@ -35,6 +35,19 @@ console.log(dfs(graph1, 'A'));
 const graph2 = [[1, 2, 4], [0, 5], [0, 5], [4], [0, 3], [1, 2]];
 console.log(dfs(graph2, 0));
 
+const graph3 = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7],
+];
+console.log(dfs(graph3, 1));
+
 /*
 참고
 https://velog.io/@sean2337/Algorithm-DFS%EC%99%80-BFS%EC%9D%98-%EC%89%AC%EC%9A%B4-%EA%B0%9C%EB%85%90-JavaScript-%EA%B5%AC%ED%98%84-%EB%B0%A9%EB%B2%95
